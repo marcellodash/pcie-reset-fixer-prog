6682d26076788d76b2b09d2a91c7817aef5abd8c
Rogerio <rogermm@gmail.com>
2019-08-15 18:03:14 -0300

    Refactoring

--- host/main.cpp
+++ host/main.cpp
@@ -6,6 +6,39 @@
 
 #include <stdio.h>
 
+int setGpu(bool gpuPower)
+{
+    SerialUsb serial;
+    PCI pci;
+
+    pci.setDriverName("pcipowerplug");
+
+    if(!pci.isDriverLoad())
+    {
+       printf("Driver not loaded");
+       return -2;
+    }
+
+    serial.setPortName("ttyACM0");
+
+    auto status = serial.open();
+
+    if(!status) {
+        return serial.getLastError();
+    }
+
+    if(!serial.setGpuPower(gpuPower)) {
+        return serial.getLastError();
+    }
+
+    if(gpuPower) {
+        printf("GPU power on\n");
+    }
+    else {
+        printf("GPU power off\n");
+    }
+}
+
 int main(int argc, char *argv[])
 {
    QCoreApplication app(argc, argv);
@@ -26,38 +59,6 @@ int main(int argc, char *argv[])
       return -1;
    }
 
-   bool gpuPower = (argv[1][0] == '1');
-
-   SerialUsb serial;
-   PCI pci;
-
-   pci.setDriverName("pcipowerplug");
-
-   if(!pci.isDriverLoad())
-   {
-      printf("Driver not loaded");
-      return -2;
-   }
-
-   serial.setPortName("ttyACM0");
-
-   auto status = serial.open();
-
-   if(!status) {
-       return serial.getLastError();
-   }
-
-   if(!serial.setGpuPower(gpuPower)) {
-       return serial.getLastError();
-   }
-
-   if(gpuPower) {
-       printf("GPU power on\n");
-   }
-   else {
-       printf("GPU power off\n");
-   }
-
-   return 0;
+   return setGpu(argv[1][0] == '1');
 }
 
